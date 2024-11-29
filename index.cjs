e463f8e0af2bec4fcc25698df0b1b601d71ddc85
Dustin Brickwood <dustinbrickwood204@gmail.com>
2024-11-29 12:35:42 -0600

    testing: rename

--- index.cjs
+++ index.cjs
@@ -327,8 +327,8 @@ async function isNodeRunning(host, port) {
 async function getDownloadUrl(releaseTag, arch) {
   const apiUrl =
     releaseTag === "latest"
-      ? "https://api.github.com/repos/matter-labs/era-test-node/releases/latest"
-      : `https://api.github.com/repos/matter-labs/era-test-node/releases/tags/${releaseTag}`;
+      ? "https://api.github.com/repos/dutterbutter/anvil-zksync/releases/latest"
+      : `https://api.github.com/repos/dutterbutter/anvil-zksync/releases/tags/${releaseTag}`;
 
   info(`Fetching release information from ${apiUrl}`);
 
