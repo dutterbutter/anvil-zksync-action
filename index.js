e51f4bf64b093244fed083c5a08697ab34c5d4b2
Dustin Brickwood <dustinbrickwood204@gmail.com>
2025-01-09 12:08:51 -0600

    testing

--- index.js
+++ index.js
@@ -374,7 +374,7 @@ async function isNodeRunning(host, port) {
 async function getDownloadUrl(releaseTag, arch) {
   const apiUrl =
     releaseTag === "latest"
-      ? "https://api.github.com/repos/matter-labs/anvil-zksync/releases/latest"
+      ? "https://api.github.com/repos/dutterbutter/anvil-zksync/releases/latest"
       : `https://api.github.com/repos/matter-labs/anvil-zksync/releases/tags/${releaseTag}`;
 
   info(`Fetching release information from ${apiUrl}`);
